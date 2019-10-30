5942065a3d203633b16a9d2cd3f84d2e31502eb3
StivenCeballos <50753058+StivenCeballos@users.noreply.github.com>
2019-10-30 14:08:29 -0500

    productos adquiridos desde el server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // importar handlebars :)
 var exphbs  = require('express-handlebars');
- 
+
+const assert= require('assert');
 
 // importar módulo
 const express = require('express');
@@ -9,12 +10,14 @@ var bodyParser = require('body-parser');
 // importar file system
 var fs = require('fs');
 
+const MongoClient = require('mongodb').MongoClient;
 // instanciar app
 const app = express();
 //lineas de handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+const createRoutes = require('./routes.js');
 
 // configuración body parser para poder usar variables post en el body
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,16 +25,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // definir puerto
 const port = 3000;
 
+const client = new MongoClient('mongodb://localhost:27017');
+client.connect((error)=>{
+    assert.equal(null,error);
+    const db= client.db('store');
+    createRoutes(app,db);
+}
+);
 // definir una carpeta como pública
 app.use(express.static('public'));
 
-app.get('/', (request, response)=>{
-    response.sendFile(__dirname+'/public/splashpage/index.html');
-});
 
-app.get('/tienda', (request, response)=>{
-    response.render('products');
-}
-);
 
 app.listen(port);
---
+++ routes.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const ObjectID = require('mongodb').ObjectID;
+
+function createRoutes(app,db){
+    app.get('/', (request, response)=>{
+        response.sendFile(__dirname+'/public/splashpage/index.html');
+    });
+    
+    app.get('/tienda', (request, response)=>{
+        
+        var products = db.collection('products').find({})
+            .toArray((error, result) => {
+                assert.equal(null,error);
+
+                var context = {
+                    products: result
+                };
+
+                response.render('products',context);
+            });
+    }
+    );
+
+}
+
+module.exports=createRoutes;
